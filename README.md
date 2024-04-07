f4900a19757a6e0b3744b0d64fd1b9157c3daa09
Jago de Vreede <jagodevreede@gmail.com>
2024-04-07 20:18:36 +0200

    Show only installed versions

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 `sdk u java 22.1.0.1.r17-gln`
 `export GRAALVM_HOME=$JAVA_HOME`
-`mvn clean install -DskipTests`
+`./mvnw clean install -DskipTests`
 `./mvnw gluonfx:build -f sdkman-ui`
--- sdkman-ui/src/main/java/io/github/jagodevreede/sdkmanui/MainScreenController.java
+++ sdkman-ui/src/main/java/io/github/jagodevreede/sdkmanui/MainScreenController.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -24,6 +25,8 @@ public class MainScreenController implements Initializable {
     Label selected_item_label;
     @FXML
     Label global_version_label;
+    @FXML
+    CheckBox showInstalledOnly;
 
     ObservableList<JavaVersionView> tableData;
 
@@ -32,23 +35,29 @@ public class MainScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         table.getColumns().clear();
-
         api = ServiceRegistry.INSTANCE.getApi();
+
+        TableColumn<JavaVersionView, String> vendorCol = getTableColumn("Vendor", "vendor");
+        TableColumn<JavaVersionView, String> lastNameCol = getTableColumn("Version", "version");
+        TableColumn<JavaVersionView, String> emailCol = getTableColumn("Dist", "dist");
+        TableColumn<JavaVersionView, String> identifierCol = getTableColumn("Identifier", "identifier");
+        TableColumn<JavaVersionView, String> installedCol = getTableColumn("installed", "installed");
+        TableColumn<JavaVersionView, String> actionCol = getTableColumn("actions", "actions");
+
+        table.getColumns().addAll(vendorCol, lastNameCol, emailCol, identifierCol, installedCol, actionCol);
+        loadData();
+        showInstalledOnly.selectedProperty().addListener((observable, oldValue, newValue) -> loadData());
+    }
+
+    private void loadData() {
         try {
             String javaGlobalVersionInUse = api.resolveCurrentVersion("java");
             setGlobalVersionLabel(javaGlobalVersionInUse);
             tableData = FXCollections.observableArrayList(
-                    api.getJavaVersions().stream().map(j -> new JavaVersionView(j, javaGlobalVersionInUse)).toList()
+                    api.getJavaVersions().stream()
+                            .filter(j -> !showInstalledOnly.isSelected() || j.installed())
+                            .map(j -> new JavaVersionView(j, javaGlobalVersionInUse)).toList()
             );
-
-            TableColumn<JavaVersionView, String> vendorCol = getTableColumn("Vendor", "vendor");
-            TableColumn<JavaVersionView, String> lastNameCol = getTableColumn("Version", "version");
-            TableColumn<JavaVersionView, String> emailCol = getTableColumn("Dist", "dist");
-            TableColumn<JavaVersionView, String> identifierCol = getTableColumn("Identifier", "identifier");
-            TableColumn<JavaVersionView, String> installedCol = getTableColumn("installed", "installed");
-            TableColumn<JavaVersionView, String> actionCol = getTableColumn("actions", "actions");
-
-            table.getColumns().addAll(vendorCol, lastNameCol, emailCol, identifierCol, installedCol, actionCol);
 
             table.setItems(tableData);
         } catch (IOException | InterruptedException e) {
@@ -73,7 +82,7 @@ public class MainScreenController implements Initializable {
     }
 
     public void javaSelected(ActionEvent event) {
-      //  table.set
+        //  table.set
     }
 
     public void mavenSelected(ActionEvent event) {
