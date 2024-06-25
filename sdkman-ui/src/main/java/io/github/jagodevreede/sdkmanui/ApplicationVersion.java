6fcbb982d900b2dd5eb617dbe1a1087f73157a9b
Jago de Vreede <jagodevreede@gmail.com>
2024-06-25 09:38:18 +0200

    Don't use resource path in native images, also copy version.txt on install

--- sdkman-ui/src/main/java/io/github/jagodevreede/sdkmanui/ApplicationVersion.java
+++ sdkman-ui/src/main/java/io/github/jagodevreede/sdkmanui/ApplicationVersion.java
@@ -1,10 +1,8 @@
 package io.github.jagodevreede.sdkmanui;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public final class ApplicationVersion {
 
@@ -16,9 +14,10 @@ public final class ApplicationVersion {
         // hidden constructor as it is a static instance.
 
         try {
-            final List<String> lines = Files.readAllLines(Paths.get(ApplicationVersion.class.getClassLoader().getResource("version.txt").toURI()));
-            version = lines.get(0);
-        } catch (IOException | URISyntaxException e) {
+            InputStream resourceAsStream = ApplicationVersion.class.getClassLoader()
+                .getResourceAsStream("version.txt");
+            version = new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
             version = "unknown";
         }
 
