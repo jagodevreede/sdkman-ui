7257b2d57ca0f5293e43912d424887e587b7c1a3
Koen Aben <koen.aben@carapaxit.nl>
2025-02-25 07:51:43 +0100

    [windows] made CandidateListParser.java work on windows

--- sdkman-api/src/main/java/io/github/jagodevreede/sdkman/api/parser/CandidateListParser.java
+++ sdkman-api/src/main/java/io/github/jagodevreede/sdkman/api/parser/CandidateListParser.java
@@ -7,23 +7,106 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.Objects.requireNonNull;
+
 public final class CandidateListParser {
-    private static final Pattern CANDIDATE_PATTERN = Pattern.compile("---\\n(.+?)\\(.*?\\n\\n(.*?)\\$ sdk install(.*?)\\n", Pattern.MULTILINE | Pattern.DOTALL);
 
     private CandidateListParser() {
     }
 
     public static List<Candidate> parse(String response) {
-        var result = new ArrayList<Candidate>();
-        Matcher matcher = CANDIDATE_PATTERN.matcher(response);
+        // NB: on windows the regular expression parser fails.
 
-        while (matcher.find()) {
-            String name = matcher.group(1).trim();
-            String description = matcher.group(2).trim().replace("\n", " ");
-            String id = matcher.group(3).trim();
-            result.add(new Candidate(id, name, description));
+        if (isWindows()) {
+            return SdkManOutputTextParser.parse(response);
         }
 
-        return result;
+        return LinuxTextParser.parse(response);
+    }
+
+    static class SdkManOutputTextParser {
+        private SdkManOutputTextParser(){
+
+        }
+        static List<Candidate>  parse(String response) {
+            List<Candidate> result = new ArrayList<>();
+            String[] lines = response.split("\n");
+            CandidateBuilder current = null;
+            for (String line: lines) {
+                var lineParser = new LineParser(line);
+
+                if (lineParser.isStartLine()) {
+                    if (current!=null) {
+                        result.add(current.build());
+                    }
+                    current = new CandidateBuilder();
+                } else
+                if (lineParser.isNameLine()&&current!=null) {
+                    current.name = lineParser.readName();
+                } else
+                if (lineParser.isEndLine()&&current!=null) {
+                    current.id = lineParser.readId();
+                }
+                else {
+                    if (current!=null) {
+                        current.sb.append(line).append(" ");
+                    }
+                }
+            }
+            return result;
+        }
+        private static class CandidateBuilder {
+            private String id;
+            private String name;
+            private final StringBuilder sb = new StringBuilder();
+
+            public Candidate build() {
+                return new Candidate(requireNonNull(id,"id should not be null"), requireNonNull(name,"name should not be null"), sb.toString());
+            }
+        }
+        private static class LineParser {
+            private final String line;
+            LineParser(String line) {
+                this.line = line;
+            }
+            public boolean isStartLine() {
+                return line.contains("----");
+            }
+            public boolean isNameLine() {
+                return line.contains("https://") ||line.contains("http://");
+            }
+            public boolean isEndLine() {
+                return line.contains("$ sdk install ");
+            }
+            public String readName() {
+                return line.substring(0, line.indexOf("(")).trim();
+            }
+            public String readId() {
+                int index = line.indexOf("$ sdk install ") +14;
+                return line.substring(index).trim();
+            }
+        }
+    }
+
+    private static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("windows");
+    }
+
+    private static class LinuxTextParser {
+        private static final Pattern CANDIDATE_PATTERN = Pattern.compile("---\\n(.+?)\\(.*?\\n\\n(.*?)\\$ sdk install(.*?)\\n", Pattern.MULTILINE | Pattern.DOTALL);
+
+        public static List<Candidate> parse(String response) {
+            var result = new ArrayList<Candidate>();
+            Matcher matcher = CANDIDATE_PATTERN.matcher(response);
+
+            while (matcher.find()) {
+                String name = matcher.group(1).trim();
+                String description = matcher.group(2).trim().replace("\n", " ");
+                String id = matcher.group(3).trim();
+                result.add(new Candidate(id, name, description));
+            }
+
+            return result;
+        }
     }
 }
--- sdkman-api/src/test/java/io/github/jagodevreede/sdkman/api/parser/CandidateListParserTest.java
+++ sdkman-api/src/test/java/io/github/jagodevreede/sdkman/api/parser/CandidateListParserTest.java
@@ -1,21 +1,57 @@
 package io.github.jagodevreede.sdkman.api.parser;
 
+import io.github.jagodevreede.sdkman.api.domain.Candidate;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 import static io.github.jagodevreede.sdkman.api.parser.TestHelper.loadFileFromResource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CandidateListParserTest {
+    private static final Logger LOG = LoggerFactory.getLogger(CandidateListParserTest.class);
 
     @Test
     void parse() throws Exception {
-        String resource = loadFileFromResource("api_examples/candidates/list/out.txt");
-        var candidates = CandidateListParser.parse(resource);
-        assertThat(candidates).isNotNull().hasSize(72);
+        String resource = TestHelper.readSdkManOutput();
+        List<Candidate> candidates = CandidateListParser.parse(resource);
+        assertThat(candidates).isNotNull();
+        if (candidates.isEmpty()) {
+            LOG.info("resource {}", resource);
+            throw new IllegalStateException("empty collection found: " +resource);
+        }
+        TestHelper.validateParsed(candidates);
+    }
 
-        assertThat(candidates.get(0).id()).isEqualTo("activemq");
-        assertThat(candidates.get(0).name()).isEqualTo("Apache ActiveMQ");
-        assertThat(candidates.get(0).description()).contains("ubiquitous AMQP protocol");
+    @Test
+    void parseForWindows() throws Exception {
+        String resource = TestHelper.readSdkManOutput();
+        List<Candidate> candidates = CandidateListParser.SdkManOutputTextParser.parse(resource);
+
+        TestHelper.validateParsed(candidates);
+    }
+
+    private static class TestHelper {
+        private static String readSdkManOutput() throws IOException {
+            return loadFileFromResource("api_examples/candidates/list/out.txt");
+        }
+
+        private static void validateParsed(List<Candidate> candidates) {
+            assertThat(candidates).hasSize(72);
+
+            assertThat(candidates.get(0).id()).isEqualTo("activemq");
+            assertThat(candidates.get(0).name()).isEqualTo("Apache ActiveMQ");
+            assertThat(candidates.get(0).description()).contains("ubiquitous AMQP protocol");
+
+            candidates.forEach( candidate -> {
+                assertThat(candidate.id()).isNotNull().isNotEmpty();
+                assertThat(candidate.name()).isNotNull().isNotEmpty();
+                assertThat(candidate.description()).isNotNull().isNotEmpty();
+            });
+        }
     }
 
 }
